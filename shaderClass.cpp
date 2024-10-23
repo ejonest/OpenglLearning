59c1ccbcbd2ea9aac94b719b58b31f1502ac2fcc
UserError <ethanjones421@yahoo.com>
2024-10-23 11:20:11 -0700

    Finished tutorial 5 (shaders). Updated shader code to have a gradiant color using in and out variables, added a uniform varible to adjust the size, and added more helpful error checking to shaderClass

--- shaderClass.cpp
+++ shaderClass.cpp
@@ -31,16 +31,19 @@ Shader::Shader(const char* vertexFile, const char* fragmentFile)
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
 	glCompileShader(vertexShader);
+	compileErrors(vertexShader, "VERTEX");
 	// fragment
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
 	glCompileShader(fragmentShader);
+	compileErrors(fragmentShader, "FRAGMENT");
 	// Wrap up the two shaders into a shader program that will run both
 	ID = glCreateProgram();
 	// Attach the two shaders to the shader program and link the program
 	glAttachShader(ID, vertexShader);
 	glAttachShader(ID, fragmentShader);
 	glLinkProgram(ID);
+	compileErrors(ID, "PROGRAM");
 	// Now that they have been combinded and saved we may delete the orginals to keep things clean
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
@@ -54,4 +57,27 @@ void Shader::Activate()
 void Shader::Delete()
 {
 	glUseProgram(ID);
+}
+
+void Shader::compileErrors(unsigned int shader, const char* type)
+{
+	GLint hasCompiled;
+	char infoLog[1024];
+	if (type != "PROGRAM")
+	{
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &hasCompiled);
+		if (hasCompiled == GL_FALSE)
+		{
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "SHADER_COMPILATION_ERROR for:" << type << "\n" << std::endl;
+		}
+	}
+	else {
+		glGetProgramiv(shader, GL_COMPILE_STATUS, &hasCompiled);
+		if (hasCompiled == GL_FALSE)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "SHADER_LINKING_ERROR for:" << type << "\n" << std::endl;
+		}
+	}
 }
